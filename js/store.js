e8f5b3d73e8366f3799dedb75804e8e33418bc45
ecarlson1201 <eric.paul.carlson@gmail.com>
2018-09-10 15:10:53 -0700

    store improve

--- js/store.js
+++ js/store.js
@@ -1,29 +1,26 @@
 // counter that tracks current question and correct answer count, adds to question and correct answer count, and resets count
 const counter = (function () {
-    let counterModule = {};
-    let currentQuestion = 0;
-    let currentCorrect = 0
+    this.currentQuestion = 0;
+    this.currentCorrect = 0
 
-    counterModule.nextQuestion = function () {
+    this.nextQuestion = function () {
         return currentQuestion++
     };
 
-    counterModule.addCorrect = function () {
+    this.addCorrect = function () {
         return currentCorrect++
     };
 
-    counterModule.currentQuestion = function () {
+    this.currentQuestion = function () {
         return currentQuestion
     };
 
-    counterModule.currentCorrect = function () {
+    this.currentCorrect = function () {
         return currentCorrect
     };
 
-    counterModule.reset = function () {
+    this.reset = function () {
         currentQuestion = 0;
         currentCorrect = 0;
     };
-
-    return counterModule
 })();
