86de6bfcb48da6af3abf8f97fc2354152c4e9d85
ecarlson1201 <eric.paul.carlson@gmail.com>
2018-09-10 23:49:59 -0700

    store rework

--- js/quizFunctions.js
+++ js/quizFunctions.js
@@ -1,9 +1,9 @@
 'use strict';
 // displays the current question number out of total questions and total number correct
 const displayQuestionNumber = function () {
-    HEADER.text(`Question ${counter.currentQuestion} of ${questionsList.length} (${counter.currentCorrect} correct)`);
-    if (counter.currentQuestion > questionsList.length) {
-        HEADER.text(`Question ${questionsList.length} of ${questionsList.length} (${counter.currentCorrect} correct)`);
+    HEADER.text(`Question ${counter.currentQuestion()} of ${questionsList.length} (${counter.currentCorrect()} correct)`);
+    if (counter.currentQuestion() > questionsList.length) {
+        HEADER.text(`Question ${questionsList.length} of ${questionsList.length} (${counter.currentCorrect()} correct)`);
     };
 };
 
@@ -12,34 +12,33 @@ const generateQuestion = function () {
     QUESTION.text('');
     INPUT.val('');
     displayQuestionNumber();
-    let current = questionsList[counter.currentQuestion - 1];
 
-    QUESTION.text(current.question);
+    QUESTION.text(questionsList[counter.indexOf()].question);
 
     INPUT.each(function (index) {
-        $(this).val(current.options[index]);
+        $(this).val(questionsList[counter.indexOf()].options[index]);
     });
 
     LABEL.each(function (index) {
-        $(this).text(current.options[index]);
+        $(this).text(questionsList[counter.indexOf()].options[index]);
     });
 };
 
 // displays and hides quiz navigation buttons
 const displayNavButtons = function () {
-    if (counter.currentQuestion > questionsList.length) {
+    if (counter.currentQuestion() > questionsList.length) {
         RESTART.removeClass('hidden');
         NEXT.addClass('hidden');
-        RESULT.text(`Your final score is: ${counter.currentCorrect} out of ${questionsList.length}`)
+        RESULT.text(`Your final score is: ${counter.currentCorrect()} out of ${questionsList.length}`)
         VIDEO.removeClass('hidden');
         VIDEO.prop('href', 'https://www.youtube.com/watch?v=vmNMCBW9O88');
         VIDEO.text('\"Join me in singing a Vikings Christmas Carol!\"');
     };
-    if (counter.currentQuestion === 1) {
+    if (counter.currentQuestion() === 1) {
         SOUNDER.attr('src', './assets/audio/welcometominnesota.mp3');
         VIDEO.addClass('hidden');
     };
-    if (counter.currentQuestion <= questionsList.length) {
+    if (counter.currentQuestion() <= questionsList.length) {
         RESTART.addClass('hidden');
         NEXT.removeClass('hidden');
         NEXT.text('Next Question');
@@ -53,9 +52,9 @@ const getRandomAsset = function (assetArray) {
 
 // grabs link for correspoding question
 const getVidLink = function () {
-    if (!questionsList[counter.currentQuestion - 1].link === false) {
+    if (!questionsList[counter.indexOf()].link === false) {
         VIDEO.removeClass('hidden');
-        VIDEO.prop('href', questionsList[counter.currentQuestion - 1].link);
+        VIDEO.prop('href', questionsList[counter.currentQuestion() - 1].link);
         VIDEO.append('\"Relive this Vikings memory!\"');
     };
 };
--- js/store.js
+++ js/store.js
@@ -1,18 +1,29 @@
 // counter that tracks current question and correct answer count, adds to question and correct answer count, and resets count
-const counter = (function () {
-    this.currentQuestion = 0;
-    this.currentCorrect = 0
+const Counter = function () {
+    const that = this
 
-    this.nextQuestion = function () {
-        return currentQuestion++
+    this.myCurrentQuestion = 0;
+    this.myCurrentCorrect = 0;
+    
+    this.currentQuestion = function() {
+        return this.myCurrentQuestion
     };
-
+    this.currentCorrect = function () {
+        return this.myCurrentCorrect
+    };
+    this.nextQuestion = function () {
+        this.myCurrentQuestion = that.myCurrentQuestion + 1
+    };
     this.addCorrect = function () {
-        return currentCorrect++
+        this.myCurrentCorrect = that.myCurrentCorrect + 1;
     };
     this.reset = function () {
-        this.currentQuestion = 0;
-        this.currentCorrect = 0;
-    }
-    return this
-})();
+        this.myCurrentQuestion = 0;
+        this.myCurrentCorrect = 0;
+    };
+    this.indexOf = function () {
+        return this.myCurrentQuestion - 1
+    };
+};
+
+const counter = new Counter()
